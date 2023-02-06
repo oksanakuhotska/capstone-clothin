c9f1f85918f2437a63295a4d228c4464c41ade62
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-02-06 13:54:49 +0200

    feat: create options to add auth users to DB

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
 	GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc
+} from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCvLJK5BhKbUiIPPJ024EMqe28mDHygHoM",
@@ -27,3 +34,36 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+
+	// якщо не має userSnapShot, буде створено
+	if(!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email, 
+				createAt
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+}
+
+// якщо юзер дані існуюють, то виконається цей код
