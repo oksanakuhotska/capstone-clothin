96442e7b126bcd700ab8e7c18d91a830081c245d
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-02-08 19:00:09 +0200

    refactor: change router position, add App compnt

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.style.css';
 import reportWebVitals from './reportWebVitals';
-
-import { RouterProvider } from "react-router-dom";
-import router from './router';
-import GlobalStyle from './global.styles';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-		<GlobalStyle />
+    <App />
   </React.StrictMode>
 );
 
