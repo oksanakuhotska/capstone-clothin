2e4bea88b5bdb820708a76b7ccf3f268f006fa70
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-02-09 13:13:54 +0200

    refactor: add router and userProvider(acces users)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.style.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+
+import { RouterProvider } from "react-router-dom";
+import router from './router';
+import GlobalStyle from './global.styles';
+
+import { UserProvider } from './contexts/user.context';
+
+
+import './index.style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+		<UserProvider>
+			<RouterProvider router={router} />
+			<GlobalStyle />
+		</UserProvider>
   </React.StrictMode>
 );
 
