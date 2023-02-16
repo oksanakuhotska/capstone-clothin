2b6d703eeaef65692e03a0f9bc7d371b6f250edd
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-02-16 22:48:17 +0200

    style: changed colors for variations

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -24,14 +24,14 @@ export const FormInputLabel = styled.label`
 export const Input = styled.input`
 	background: none;
 	background-color: white;
-	color: grey;
+	color: ${subColor};
 	font-size: 18px;
 	padding: 10px 10px 10px 5px;
 	display: block;
 	width: 100%;
 	border: none;
 	border-radius: 0;
-	border-bottom: 1px solid grey;
+	border-bottom: 1px solid ${subColor};
 	margin: 25px 0;
 
 	&:focus {
