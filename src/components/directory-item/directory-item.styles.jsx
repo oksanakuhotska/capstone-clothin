c7b69d003799309cae97e5eed71f0f5447368258
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-02-16 23:28:55 +0200

    styles: responsible directoty items

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -37,13 +37,17 @@ export const Body = styled.div`
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
   height: 240px;
-  flex: 1 1 auto;
+	flex: 1 0 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   border: 1px solid black;
   margin: 0 7.5px 15px;
   overflow: hidden;
+
+	@media (min-width: 497px){
+		flex: 1 1 auto;
+	}
 
   &:hover {
     cursor: pointer;
