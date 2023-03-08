13a021899192d63e1bac6e0c1116d071106b73f6
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-08 11:34:42 +0200

    refactor: context provider to redux, app router

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
-import { RouterProvider } from "react-router-dom";
-import router from './router';
-import { GlobalStyle } from './global.styles';
+import App from './App';
 
-import { UserProvider } from './contexts/user.context';
+import { store } from './store/store';
+
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
+import { GlobalStyle } from './global.styles';
 import './index.style.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-		<UserProvider>
+		<Provider store={store}>
 			<CategoriesProvider>
 				<CartProvider>
-					<RouterProvider router={router} />
+					<App />
 					<GlobalStyle />
 				</CartProvider>
 			</CategoriesProvider>
-		</UserProvider>
+		</Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
