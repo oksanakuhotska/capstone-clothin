6b0d4bb595915aab5de86bbee72f168da9ba1ece
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-09 11:44:30 +0200

    feat: changed logger-library to customize logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,25 @@
 import { compose, applyMiddleware } from "redux";
 import { legacy_createStore as createStore } from 'redux';
 
-import logger from "redux-logger"; // show state
+// import logger from "redux-logger"; // show state
 
 import { rootReducer } from "./root-reducer"; // загальний редюсер
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+	if(!action.type) {
+		return next(action);
+	}
+	
+	console.log('type', action.type);
+	console.log('payload', action.payload);
+	console.log('currentState', store.getState());
+
+	next(action);
+
+	console.log('next state', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
