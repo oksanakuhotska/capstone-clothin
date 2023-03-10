12512a769c762bb7e208a7e76075e5e6c9454d09
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 11:29:53 +0200

    refactor: chng useEffect to async middleware thrun

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { setCategories } from "../../store/categories/categories.action";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from "../category/category.component";
@@ -14,12 +13,7 @@ const Shop = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    };
-	
-		getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
 	}, []);
 
 
