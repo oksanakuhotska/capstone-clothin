10e79e0d1fbf8f69243213ebd393406b29f0f9d0
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 11:51:10 +0200

    refactor: changed blacklist to whitelist(cart)

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ import thunk from 'redux-thunk';
 const persistConfig = {
 	key: 'root',
 	storage,
-	blacklist: ['user'],
+	whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
