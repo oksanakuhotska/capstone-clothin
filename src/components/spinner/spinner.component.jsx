f35b21f456b0b2245b3a86fff6cdbbef67e729d7
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 11:33:57 +0200

    feat: created spinner component

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+
+const Spinner = () => {
+	return(
+		<SpinnerContainer>
+			<SpinnerContainer />
+		</SpinnerContainer>
+	)
+};
+
+export default Spinner;
