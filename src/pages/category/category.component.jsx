a79d4a86cbb5155c669443db47d433d476359673
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 11:44:22 +0200

    feat: added spinner loading to categories

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -3,13 +3,15 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import { selectCategoriesIsLoadding, selectCategoriesMap } from '../../store/categories/categories.selector';
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
 	const { category } = useParams();
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoadding);
 	const [products, setProducts] = useState(categoriesMap[category]);
 	
 	useEffect(() => {
@@ -19,11 +21,15 @@ const Category = () => {
 	return (
 		<>
 			<Title>{category.toUpperCase()}</Title>
-			<CategoryContainer>
-				{products &&
-					products.map((product) => <ProductCard key={product.id} product={product} />)
-				}
-			</CategoryContainer>
+			{ isLoading ? (
+				<Spinner /> 
+				) : (
+				<CategoryContainer>
+					{products &&
+						products.map((product) => <ProductCard key={product.id} product={product} />)
+					}
+				</CategoryContainer>
+			)}
 		</>
 	);
 };
