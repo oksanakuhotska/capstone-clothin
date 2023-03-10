173ef3913f1162716b8ba505ee5aeb072eb08e02
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 10:37:51 +0200

    feat: added redux trunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,37 @@
 import { compose, applyMiddleware } from "redux";
 import { legacy_createStore as createStore } from 'redux';
 
-// import logger from "redux-logger"; // show state
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from "./root-reducer"; // загальний редюсер
 
-const loggerMiddleware = (store) => (next) => (action) => {
-	if(!action.type) {
-		return next(action);
-	}
-	
-	console.log('type', action.type);
-	console.log('payload', action.payload);
-	console.log('currentState', store.getState());
+import logger from "redux-logger"; // show state
+// import { loggerMiddleware } from "./middlewaew/logger";
 
-	next(action);
+import thunk from 'redux-thunk';
 
-	console.log('next state', store.getState());
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
 };
 
-const middleWares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// const middleWares = [loggerMiddleware];
+const middleWares = [
+	process.env.NODE_ENV !== 'production' && logger, 
+	thunk // added thunk
+].filter(Boolean);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composeEnchancer = 
+	(process.env.NODE_ENV !== 'production' && 
+		window && 
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
+	compose;
+
+const composedEnhancers = composeEnchancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
