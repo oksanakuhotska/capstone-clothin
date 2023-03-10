0c4abc9d8177c3ddb4203aa87e7bd9f7fc372a99
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-10 11:44:01 +0200

    feat: create select spinner loading

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -16,3 +16,8 @@ export const selectCategoriesMap = createSelector(
 	}, {}),
 );
 
+export const selectCategoriesIsLoadding = createSelector(
+	[selectCategoryReducer],
+	(categoriesSlice) => categoriesSlice.isLoading
+)
+
