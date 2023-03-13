10860c333c3e034651e22aeaa8437c5b9dbb7406
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-13 15:52:31 +0200

    style: chnage styles for auth

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const SignInContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 380px;
-	padding: 10px;
+	max-width: 380px;
 
 	h2 {
 		margin: 10px 0;
@@ -13,5 +12,11 @@ export const SignInContainer = styled.div`
 
 export const ButtonsContainer = styled.div`
 	display: flex;
+	flex-direction: column;
 	justify-content: space-between;
+	gap: 10px;
+
+	@media (min-width: 568px) {
+		flex-direction: row;
+	}
 `;
--- src/components/sign-up-form/sign-up-form.styles.jsx
+++ src/components/sign-up-form/sign-up-form.styles.jsx
@@ -3,10 +3,14 @@ import styled from "styled-components";
 export const SignUpContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 380px;
-	padding: 10px;
+	max-width: 380px;
+	padding: 10px 0px 10px 0px;
 
 	h2 {
 		margin: 10px 0;
+	}
+
+	button {
+		width: 100%;
 	}
 `;
