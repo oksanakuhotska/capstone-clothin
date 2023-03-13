b12a547cf4c08198671faea826b3749c12f4787c
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-13 15:51:40 +0200

    style: created styles for signin-auth

---
+++ src/pages/signin-auth/signin-auth.styles.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SignInFormContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 100px auto;
+	max-width: 380px;
+	gap: 20px;
+`;
+
+export const SignInSubText = styled.div`
+	text-align: center;
+	margin-top: 50px;
+`;
+
