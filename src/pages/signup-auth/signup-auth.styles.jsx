2f82cdc532fb0541c3bee3b379954638bfb28d59
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-13 15:51:54 +0200

    style: created styles for signup-auth

---
+++ src/pages/signup-auth/signup-auth.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SignUpFormContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 50px auto;
+	max-width: 380px;
+	gap: 20px;
+`;
+
+export const SignUpSubText = styled.div`
+	text-align: center;
+	margin-top: 15px;
+`;
