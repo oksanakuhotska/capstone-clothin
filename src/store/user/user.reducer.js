8f65cdb8bef40f45b537978f764c61583348a600
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 15:16:33 +0200

    refactor: replaced action type to success&failed

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -2,17 +2,26 @@ import { USER_ACTION_TYPES } from "./user.type";
 
 const INITIAL_STATE = {
 	currentUser: null,
+	isLoading: false,
+	error: null,
 };
 
 export const userReducer = (state = INITIAL_STATE, action) => {
 	const { type, payload } = action;
 
 	switch(type) {
-		case USER_ACTION_TYPES.SET_CURRENT_USER:
+		//replaced on success type
+		case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
 			return {
 				...state,
 				currentUser: payload,
-			}
+			};
+			//added failed type
+		case USER_ACTION_TYPES.SIGN_IN_FAILED:
+			return {
+				...state, 
+				error: payload,
+			};
 		default: 
 			return state;
 	}
