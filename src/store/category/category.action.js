6e4cd23d13485b13e5a8e11118c4387a68a2d2d5
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 13:59:34 +0200

    refactor: deleted fetchCategoriesAsync

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,7 +1,7 @@
 import CATEGORIES_ACTION_TYPES from './category.types';
 
 import { createAction } from '../../utils/reducer/reducer.utils';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 export const fetchCategoriesStart = () => 
 	createAction(
@@ -20,12 +20,12 @@ export const fetchCategoriesFailed = (error) =>
 		error,
 	);
 
-	export const fetchCategoriesAsync = () => async (dispatch) => {
-		dispatch(fetchCategoriesStart());
-		try {
-			const categoriesArray = await getCategoriesAndDocuments ('categories');
-			dispatch(fetchCategoriesSuccess(categoriesArray));
-		}	catch (error) {
-			dispatch(fetchCategoriesFailed(error));
-		}
-	};
+// export const fetchCategoriesAsync = () => async (dispatch) => {
+// 	dispatch(fetchCategoriesStart());
+// 	try {
+// 		const categoriesArray = await getCategoriesAndDocuments ('categories');
+// 		dispatch(fetchCategoriesSuccess(categoriesArray));
+// 	}	catch (error) {
+// 		dispatch(fetchCategoriesFailed(error));
+// 	}
+// };
