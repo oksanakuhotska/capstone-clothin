3c4a20a0b988e53442e7782dc9cbfa64732d3186
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 14:52:00 +0200

    refactor: replaced return userSnapshot

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -94,7 +94,7 @@ export const createUserDocumentFromAuth = async (
 	}
 
 	// якщо юзер дані існуюють, то виконається цей код
-	return userDocRef;
+	return userSnapshot;
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
@@ -112,3 +112,16 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const unsubscribe = onAuthStateChanged(
+			auth, 
+			(userAuth) => {
+				unsubscribe();
+				resolve(userAuth);
+			},
+			reject
+		);
+	});
+};
