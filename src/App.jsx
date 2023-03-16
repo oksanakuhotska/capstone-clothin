d7ccf52bc203bae2345c1f07e3d4749a675ee550
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 15:14:45 +0200

    refactor: replace method in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { RouterProvider } from "react-router-dom";
 import { useDispatch } from "react-redux/es/exports";
 
 import router from "./router";
-import { getCurrentUser } from "./utils/firebase/firebase.utils";
+
+import { checkUserSesssion } from "./store/user/user.action";
 
 const App = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		getCurrentUser();
-	}, [dispatch]);
+		dispatch(checkUserSesssion());
+	}, []);
 
 	return (
 		<>
