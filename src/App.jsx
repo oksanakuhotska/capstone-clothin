c46b1a6ecc02a3f681edc894abdffaefff1fadf3
oksanakuhotska <53738384+oksanakuhotska@users.noreply.github.com>
2023-03-16 14:52:43 +0200

    refactor: migrated unsibscribe to saga

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,13 @@ import { RouterProvider } from "react-router-dom";
 import { useDispatch } from "react-redux/es/exports";
 
 import router from "./router";
-import { setCurrentUser } from "./store/user/user.action";
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
+import { getCurrentUser } from "./utils/firebase/firebase.utils";
 
 const App = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChangedListener((user) => {
-			if(user) {
-				createUserDocumentFromAuth(user);
-			}
-			dispatch(setCurrentUser(user));
-		});
-
-		return unsubscribe;
+		getCurrentUser();
 	}, [dispatch]);
 
 	return (
